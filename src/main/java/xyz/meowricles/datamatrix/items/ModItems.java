d4727597b8ae4b7a278f6c7e485a8247cb5d6e67
Griffin <griffin@edforma.app>
2025-10-28 08:26:16 -0500

    polycarbobate...

--- src/main/java/xyz/meowricles/datamatrix/items/ModItems.java
+++ src/main/java/xyz/meowricles/datamatrix/items/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import xyz.meowricles.datamatrix.Datamatrix;
+import xyz.meowricles.datamatrix.items.ingredients.PolycarbonateBlob;
 import xyz.meowricles.datamatrix.items.media.CompactDiscItem;
 import xyz.meowricles.datamatrix.items.media.MysteryNandItem;
 
@@ -13,6 +14,12 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Datamatrix.MODID);
 
+    // Ingredients
+    public static final RegistryObject<Item> POLYCARBONATE_BLOB =
+            ITEMS.register("polycarbonate_blob", () -> new PolycarbonateBlob(new Item.Properties()));
+
+
+    // Storage Mediums
     public static final RegistryObject<Item> COMPACT_DISC =
             ITEMS.register("compact_disc", () -> new CompactDiscItem(new Item.Properties()));
 
---
+++ src/main/java/xyz/meowricles/datamatrix/items/ingredients/PolycarbonateBlob.java
@@ -0,0 +1,9 @@
+package xyz.meowricles.datamatrix.items.ingredients;
+
+import net.minecraft.world.item.Item;
+
+public class PolycarbonateBlob extends Item {
+    public PolycarbonateBlob(Properties props) {
+        super(props);
+    }
+}
